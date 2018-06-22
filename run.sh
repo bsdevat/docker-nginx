3e91db81ffbf4c7e49277a8f18913d86b050dba1
Sebastian Brandner <admin@bsdev.at>
2018-06-22 20:02:54 +0200

    run script updated

--- run.sh
+++ run.sh
@@ -3,5 +3,7 @@
 sh ./remove.sh
 sh ./stop.sh
 echo Run nginx-dev as nginx-instance on http://localhost:8080 ...
-docker run -d -p 8080:8080 -p8443:8443 --name nginx-instance nginx-dev
+docker run -d \
+    -p 8080:8080 -p8443:8443 \
+    --name nginx-instance nginx-dev
 docker ps
